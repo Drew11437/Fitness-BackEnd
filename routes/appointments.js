1888097e8948bd3a545f4db99b4871dd8dcecfed
unknown <drew17509@gmail.com>
2022-03-05 12:55:56 -0700

    corrected issue with backend for scheduleing appointments so it coul match the front end axios request

--- routes/appointments.js
+++ routes/appointments.js
@@ -3,8 +3,7 @@ const {Schedule} = require("../models/schedule");
 const { User } = require('../models/user');
 const auth = require('../middleware/auth');
 const { startSession } = require('mongoose');
-
-
+const { appointmentDate} = require("../models/schedule");
 
 
 
@@ -17,20 +16,48 @@ router.get('/appointment', [auth], (req,res, next) => {
 });
 
 
-router.post("/", [auth], (req,res, next) => {
-    const{appointmentDate, time, email} = req.body;
-    if(!appointmentDate || !time || !email){
-        return res.status(400).json({
-            message: 'Appointment date, name and email are required',
-        });
+
+
+
+
+
+router.post("/", [auth], async (req, res) => {
+    try {
+      const appoint = new appointmentDate({
+        email: req.body.email,
+        appointmentDate: req.body.appointmentDate,
+        time: req.body.time,
+      });
+      await appoint.save();
+  
+      // Save it in the user's list
+      const signedInUser = await User.findById(req.user._id);
+  
+      console.log("new appointment", appoint);
+      console.log(signedInUser);
+      signedInUser.myList.push(appoint._id);
+      await signedInUser.save();
+  
+      return res.send(appoint);
+    } catch (ex) {
+      console.log(ex);
+      return res.status(500).send(`Internal Server Error: ${ex}`);
     }
-    const payload ={appointmentDate, time, email};
-    req.collection.insertOne(payload)
-    .then(result => res.json(result))
-    .catch(error=> res.status(400).json(
-       { message: 'No appointments avaliable on that date'},
-    ));
-});
+  });
+// router.post('/', [auth], (req,res, next) => {
+//     const{appointmentDate, time, email} = req.body;
+//     if(!appointmentDate || !time || !email){
+//         return res.status(400).json({
+//             message: 'Appointment date, name and email are required',
+//         });
+//     }
+//     const payload ={appointmentDate, time, email};
+//     req.collection.insertOne(payload)
+//     .then(result => res.json(result))
+//     .catch(error=> res.status(400).json(
+//        { message: 'No appointments avaliable on that date'},
+//     ));
+// });
 
 
 router.delete('/appointment/:id',(req,res, next) => {
